9d397e0eaacbe85b0191159eb2f787218a55d7a4
kevinsmithseven <kevinsmithseven@gmail.com>
2023-11-21 17:20:24 -0600

    updated App.jsx with basic layout, created Footer.jsx component

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,20 @@
+import { Outlet } from "react-router-dom";
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
+function App() {
 
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
