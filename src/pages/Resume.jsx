1d5575b43693a6669f22a7831f62eefa3d0a4323
kevinsmithseven <kevinsmithseven@gmail.com>
2023-11-26 11:08:28 -0600

    updated Resume page and added associated styles in Resume.css

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,23 +1,37 @@
+import '../styles/Resume.css'
+
 export default function Resume() {
-    return (
-      <div>
-        <h1>Blog Page</h1>
-        <p>
-          Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
-          Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
-          dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
-          sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc,
-          sed pretium risus rutrum eget. Nam consequat, ligula in faucibus
-          vestibulum, nisi justo laoreet risus, luctus luctus mi lacus sit amet
-          libero. Class aptent taciti sociosqu ad litora torquent per conubia
-          nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-          lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
-          Donec placerat accumsan mi, ut congue neque placerat eu. Donec nec ipsum
-          in velit pellentesque vehicula sit amet at augue. Maecenas aliquam
-          bibendum congue. Pellentesque semper, lectus non ullamcorper iaculis,
-          est ligula suscipit velit, sed bibendum turpis dui in sapien.
-        </p>
+  return (
+    <div className="resume-container">
+      <h1 className="resume-heading">Resume</h1>
+      <a className="resume-download-link" href="https://drive.google.com/file/d/1S-NYQLmm0KQT3GP03GEUgx0DuUxXojlR/view?usp=sharing" target="_blank" rel="noopener noreferrer" aria-label="Link to Kevin Smith's Resume">
+        Download my Resume
+      </a>
+      <div className="proficiency-section">
+        <h2>Front End Proficiencies</h2>
+        <ul>
+          <li>HTML/CSS</li>
+          <li>Javascript/jQuery</li>
+          <li>Mobile Responsiveness</li>
+          <li>Bootstrap</li>
+          <li>Progressive Web Applications</li>
+          <li>Local Storage/Session Storage/IndexedDB</li>
+          <li>MERN Stack (React.js)</li>
+        </ul>
       </div>
-    );
-  }
-  
+      <div className="proficiency-section">
+        <h2>Back End Proficiencies</h2>
+        <ul>
+          <li>MySQL</li>
+          <li>MongoDB/Mongoose</li>
+          <li>User Authentication</li>
+          <li>MERN Stack (Express.js, Node.js)</li>
+          <li>API Design (Rest, GraphQL)</li>
+          <li>Template Engines</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+
