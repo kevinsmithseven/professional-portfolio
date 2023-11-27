96c6016c94a81619914b3bb38f07d999ffda4a20
kevinsmithseven <kevinsmithseven@gmail.com>
2023-11-27 15:15:54 -0600

    added fourth link to footer, updated and cleaned up comments

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,7 @@ import '../styles/Footer.css'
 import { BsGithub } from "react-icons/bs";
 import { BsLinkedin } from "react-icons/bs";
 import { FaRegSmileWink } from "react-icons/fa";
+import { SiLetterboxd } from "react-icons/si";
 
 
 export default function Footer() {
@@ -12,6 +13,9 @@ export default function Footer() {
             </a>
             <a href='https://www.linkedin.com/in/kevinsmithseven/' target="_blank" rel="noopener noreferrer" aria-label="Link to Kevin Smith's LinkedIn profile">
                 <BsLinkedin />
+            </a>
+            <a href='https://letterboxd.com/kevinsmithseven/' target="_blank" rel="noopener noreferrer" aria-label="Link to Kevin Smith's Letterboxd profile">
+                <SiLetterboxd />
             </a>
             <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target="_blank" rel="noopener noreferrer" aria-label="Hello Rick">
                 <FaRegSmileWink />
